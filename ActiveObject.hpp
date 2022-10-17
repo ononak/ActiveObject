ef04406ae4896b5998537a6d8a77926250ff875d
Önder Nazım Onak <ononak@gmail.com>
2022-10-17 21:12:01 +0200

    Update ActiveObject.hpp

--- ActiveObject.hpp
+++ ActiveObject.hpp
@@ -3,25 +3,58 @@
 #pragma once
 
 #include "TsQueue.hpp"
+#include <condition_variable>
+#include <iostream>
+#include <thread>
 
-template<typename T> class ActiveObject{
+template <typename T> class ActiveObject {
 private:
-TsQueue<T> safeQueue;
+  TsQueue<T> safeQueue;
+  std::condition_variable cv;
+  std::mutex protector;
+  std::thread processor;
+  bool shutdown;
 
 public:
-ActiveObject() {
+  ActiveObject() : shutdown(false) {
+    processor = std::thread(&ActiveObject<T>::dataLoop, this);
+  }
+  virtual ~ActiveObject() {
+    std::cout << "ActiveObject destruct" << std::endl;
+    shutdown = true;
+    cv.notify_one();
+    processor.join();
+  }
+  void process(T &obj) {
+    safeQueue.push(obj);
+    std::cout << "Number of the data in the loop is" << safeQueue.size()
+              << std::endl;
+    cv.notify_one();
+  }
 
-}
+protected:
+  void dataLoop() {
+    std::unique_lock<std::mutex> lock(protector);
 
-virtual ~ActiveObject() {
+    while (!shutdown) {
+      if (safeQueue.empty()) {
+        std::cout << "Waiting for data..." << std::endl;
+        cv.wait(lock);
+      }
 
-}
-ActiveObject(const ActiveObject&) = delete;
-ActiveObject(const ActiveObject&&) = delete;
-ActiveObject& operator=(const ActiveObject&) = delete;
+      if (!shutdown) {
+        auto data = safeQueue.front();
+        run(data);
+      }
+    }
+  }
 
+  virtual void run(T &obj) {}
 
+private:
+  ActiveObject(const ActiveObject &) = delete;
+  ActiveObject(const ActiveObject &&) = delete;
+  ActiveObject &operator=(const ActiveObject &) = delete;
 };
-
 
 #endif // ACTIVE_OBJECT_HPP
