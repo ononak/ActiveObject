9dcc357ec46bc6a7b6d58a3a38fcf019474e3e58
Önder Nazım Onak <ononak@gmail.com>
2022-10-17 21:13:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "ActiveObject.hpp"
+#include "TsQueue.hpp"
+#include <cstdio>
 #include <iostream>
-#include "ActiveObject.hpp"
+#include <string>
+#include <thread>
+#include <unistd.h>
 
-int main(int, char**) {
+template <typename T> class DummyActiveObject : public ActiveObject<T> {
 
-ActiveObject<int> aoInt;
+protected:
+  void run(T &obj) override {
+    std::cout << " Running for data  = " << obj << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+};
 
-    std::cout << "Hello, world!\n";
+int main(int, char **) {
+
+  DummyActiveObject<int> dummyWorker;
+  int n = 20;
+  for (auto i = 1; i < n; i++) {
+    dummyWorker.process(i);
+  }
+
+  sleep(n + 1);
+  std::cout << "Ending " << std::endl;
 }
